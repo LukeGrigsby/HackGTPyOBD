6a0406ac6755af3cbe895d386edadf9a6f667251
LukeGrigsby <43653315+LukeGrigsby@users.noreply.github.com>
2018-10-21 02:51:42 -0400

    Add files via upload

---
+++ PyOBDimplementation.py
@@ -0,0 +1,80 @@
+import obd
+import time
+import csv
+
+
+class SpeedandEfficiency():
+    def _init_(self, speed, efficiency):
+        self.speed = speed
+        self.efficiency = efficiency
+SEList = []
+maxSE = SpeedandEfficiency(0.0,0.0)
+num_loaded = 0
+num_new = 0
+#Records data for speed efficiency calculations
+def EfficiencyButtonRecord():
+    valueRecorded = False
+    fuelRateTotal = 0
+    speedTotal = 0
+    fuelRateCounter = 0
+    speedCounter = 0
+    minSpeed = 0
+    maxSpeed = 0
+    def new_spd(obdqueryrefer) :
+        speedquery = obdqueryrefer.value.magnitude
+        if speedQuery < minSpeed : minSpeed = speedQuery
+        if speedQuery > maxSpeed : maxSpeed = speedQuery
+        speedTotal += speedquery
+        speedCounter += 1
+    def new_fr(obdqueryrefer) :
+        fuelratequery = obdqueryrefer.value.magnitude
+        fuelRateTotal += fuelratequery
+        fuelRateCounter += 1
+    connectionfr = obd.Async()
+    connections = obd.Async()
+    connectionfr.watch(obd.commands.FUEL_RATE, callback=new_fr)
+    connections.watch(obd.commands.SPEED, callback=new_spd)
+    connectionfr.start()
+    connections.start()
+    time.sleep(10)
+    speedAverage = (SpeedTotal/speedCounter)
+    averageConsumption = (fuelRateTotal/fuelRateCounter)/speedAverage #liters per mile
+    if ((maxSpeed - minSpeed) < 3) :
+        addValue = SpeedandEfficiency(speedAverage, averageConsumption)
+        SEList.append(addValue)
+        valueRecorded = True
+    return valueRecorded
+#keep track of num_loaded for exit func
+def StartupSEList(num_loaded_):
+    with open("PyOBDFuelEconomy.csv", newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter = ',')
+        line_count = 0
+        for row in filereader :
+            if(line_count == 0) :
+                line_count += 1
+            elif(line_count == 1) :
+                maxSE.speed = float(row[0])
+                maxSE.efficiency = float(row[1])
+                line_count += 1
+            else :
+                SEList.append(SpeedandEfficiency(float(row[0]),float(row[1])))
+                num_loaded_ += 1
+                line_count += 1
+    return num_loaded_
+def ExitSEList(num_new_):
+    with open("PyOBDFuelEconomy.csv", 'a', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for x in range(num_new_) :
+            filewriter.writerow([SEList[num_loaded + x].speed, SEList[num_loaded + x].efficiency])
+#pass a SpeedandEfficiency object
+#have to manually update MaxSE this only returns the new max
+#good practice I guess
+def UpdateEMax(currentMax):
+    for i in SEList :
+        if SEList[i].efficiency < currentMax.efficiency :
+            currentMax = SEList[i]
+    return currentMax
+#current list format
+#SPEED,EFFICIENCY //Format marker
+#CURRENT,MAX // max efficiency point
+#DATA,POINTS // list of all efficiency points
